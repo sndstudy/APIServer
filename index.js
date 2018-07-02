bf1cf5e6fe19d82800aa5d55b6cc4e898c58de6d
sndstudy <sanada2054tech@gmail.com>
2018-07-02 22:56:43 +0900

    静的ファイルの提供 追加

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let express = require('express');
 let app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+//静的ファイルの提供
+app.use(express.static('src'));
+
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
